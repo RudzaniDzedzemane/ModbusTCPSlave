0aa36cd25cb296a8b4ce38dd4a5a9aaaf14f702e
Rudzani <42094665+RudzaniDzedzemane@users.noreply.github.com>
2023-02-19 22:14:59 +0200

    Add files via upload

---
+++ ModbusTCPSlave.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ModbusTCPSlave
+{
+    public class ModbusSlave
+    {
+        // Parameters
+
+        Int32 port;
+
+        IPAddress ServerIPAddress;
+
+        TcpClient client;
+
+
+        public ModbusSlave(String ServerIPAddressString, Int32 port)
+        {
+
+            this.port = port;
+            this.ServerIPAddress = IPAddress.Parse(ServerIPAddressString);
+
+            client = new TcpClient();
+
+        }
+
+        public void OpenConnectionAndprintMessages()
+        {
+
+            Console.WriteLine("Attempting to connect to server");
+
+            client.Connect(this.ServerIPAddress, this.port);
+
+            Console.WriteLine("Connection Sucessful");
+
+            //wait for 15 seconds
+
+            Thread.Sleep(15000);
+
+            Console.WriteLine("Closing Connection");
+
+            client.Close();
+
+        }
+
+        void CloseConnection()
+        {
+            client.Close();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ModbusTCPSlave;
+
+ModbusSlave slave = new ModbusSlave("10.0.0.4", 502);
+
+slave.OpenConnectionAndprintMessages();
+
+Console.ReadKey();
